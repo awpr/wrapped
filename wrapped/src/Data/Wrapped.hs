3190e537bdef7b781ee451f25475c5738a9987b0
Andrew Pritchard <awpritchard@gmail.com>
2023-03-12 13:01:43 -0700

    Bump version bounds, update CI matrix, and fix an import warning on GHC 9.6.1.

--- wrapped/src/Data/Wrapped.hs
+++ wrapped/src/Data/Wrapped.hs
@@ -1,4 +1,5 @@
 -- Copyright 2019-2021 Google LLC
+-- Copyright 2023 Andrew Pritchard
 --
 -- Licensed under the Apache License, Version 2.0 (the "License");
 -- you may not use this file except in compliance with the License.
@@ -42,7 +43,9 @@ module Data.Wrapped
            , GSemigroup(..), GMonoid(..)
          ) where
 
+#if !MIN_VERSION_base(4,18,0)
 import Control.Applicative (liftA2)
+#endif
 import qualified Data.Foldable as F (toList)
 import Data.Function (on)
 import Data.Kind (Constraint, Type)
